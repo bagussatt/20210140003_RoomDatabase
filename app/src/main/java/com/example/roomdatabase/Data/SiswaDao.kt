e44b610addd7ea277a269b1613e236ef1907f9c0
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2023-12-06 20:13:28 +0700

    Menambahkan Metode Update pada SiswaDao.kt

--- app/src/main/java/com/example/roomdatabase/Data/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/Data/SiswaDao.kt
@@ -3,9 +3,13 @@ package com.example.roomdatabase.Data
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Update
 
 @Dao
 interface SiswaDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertSiswa(siswa: Siswa)
+    suspend fun insertSiswa(siswa: Siswa)\
+
+    @Update
+    suspend fun update(siswa: Siswa)
 }
