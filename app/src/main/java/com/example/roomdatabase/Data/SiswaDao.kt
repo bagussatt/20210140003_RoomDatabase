79f9ee84cf666f09261ba0cb0b74a1d200450f8f
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2023-12-06 20:12:18 +0700

    Menambahkan Metode Insert Siswa pada SiswaDao.kt

--- app/src/main/java/com/example/roomdatabase/Data/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/Data/SiswaDao.kt
@@ -1,2 +1,11 @@
 package com.example.roomdatabase.Data
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+
+@Dao
+interface SiswaDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertSiswa(siswa: Siswa)
+}
