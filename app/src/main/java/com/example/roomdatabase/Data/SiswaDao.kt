d680829deaa1717bae5d05f2a29fab580445dd2b
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2023-12-06 20:14:50 +0700

    Menambahkan Metode Delete pada SiswaDao.kt

--- app/src/main/java/com/example/roomdatabase/Data/SiswaDao.kt
+++ app/src/main/java/com/example/roomdatabase/Data/SiswaDao.kt
@@ -1,6 +1,7 @@
 package com.example.roomdatabase.Data
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Update
@@ -8,8 +9,11 @@ import androidx.room.Update
 @Dao
 interface SiswaDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertSiswa(siswa: Siswa)\
+    suspend fun insertSiswa(siswa: Siswa)
 
     @Update
     suspend fun update(siswa: Siswa)
+
+    @Delete
+    suspend fun delete(siswa: Siswa)
 }
