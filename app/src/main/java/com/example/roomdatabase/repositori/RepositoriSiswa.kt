d05a822c2f2b9b7fef5838aa43d2d28a6ed0bc6e
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2023-12-06 20:29:32 +0700

    Menambahkan Interface RepositoriSiswa dengan Metode untuk Mendapatkan Semua Data Siswa

--- app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/RepositoriSiswa.kt
@@ -1,2 +1,8 @@
 package com.example.roomdatabase.repositori
 
+import com.example.roomdatabase.Data.Siswa
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoriSiswa {
+    fun getAllSiswa(): Flow<List<Siswa>>
+}
