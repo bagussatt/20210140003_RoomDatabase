941ba17a738846af6cfeb9e2a6d324ec6c30276b
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2023-12-06 20:23:03 +0700

    Menambahkan Metode untuk Mendapatkan Instance DatabaseSiswa

--- app/src/main/java/com/example/roomdatabase/Data/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/Data/DatabaseSiswa.kt
@@ -1,9 +1,23 @@
 package com.example.roomdatabase.Data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Siswa::class], version = 1)
 abstract class DatabaseSiswa : RoomDatabase() {
     abstract fun siswaDao(): SiswaDao
+
+    companion object{
+        @Volatile
+        private var Instance: DatabaseSiswa? = null
+
+        fun getDatabase(context: Context): DatabaseSiswa {
+            return (Instance?: synchronized(this){
+                Room.databaseBuilder(context,DatabaseSiswa::class.java,"siswa_database")
+                    .build().also { Instance=it }
+            })
+        }
+    }
 }
