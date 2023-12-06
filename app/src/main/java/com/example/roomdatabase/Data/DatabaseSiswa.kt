57b19601bbeade589bb324d78b4a138b34f2ce0a
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2023-12-06 20:21:55 +0700

    Menambahkan Kelas DatabaseSiswa untuk Room Database dengan Entitas Siswa

--- app/src/main/java/com/example/roomdatabase/Data/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomdatabase/Data/DatabaseSiswa.kt
@@ -1,2 +1,9 @@
 package com.example.roomdatabase.Data
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Siswa::class], version = 1)
+abstract class DatabaseSiswa : RoomDatabase() {
+    abstract fun siswaDao(): SiswaDao
+}
