19dc2ff49b68344a46fab1c6ce179dbd89111403
Bagus Satrio <bagus.satrio.ft21@mail.umy.ac.id>
2023-12-06 21:35:38 +0700

    Menambahkan HomeViewModel untuk Manajemen Data Siswa di Layar Utama

--- app/src/main/java/com/example/roomdatabase/model/HomeViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/model/HomeViewModel.kt
@@ -1,2 +1,25 @@
 package com.example.roomdatabase.model
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.roomdatabase.Data.Siswa
+import com.example.roomdatabase.repositori.RepositoriSiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class HomeViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel(){
+
+    companion object{
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+    data class HomeUiState(
+        val listSiswa: List<Siswa> = listOf()
+    )
+
+    val homeUiState: StateFlow<HomeUiState> = repositoriSiswa.getAllSiswaStream().filterNotNull().map { HomeUiState(listSiswa = it.toList()) }.stateIn(scope = viewModelScope, started = SharingStarted.WhileSubscribed(
+        TIMEOUT_MILLIS), initialValue = HomeUiState())
+}
